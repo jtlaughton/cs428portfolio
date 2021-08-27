4195f9b527450d3b7a8f2a12e0fe09aafc23d5be
jtlaughton <60359046+jtlaughton@users.noreply.github.com>
2021-08-27 03:42:38 -0500

    Initial Setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+import {Container, Navbar, Nav} from 'react-bootstrap';
+import {LinkContainer} from 'react-router-bootstrap'
+
+
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <Navbar bg="dark" variant='dark' expand="lg">
+          <Container>
+            <LinkContainer to='/cs428portfolio/'>
+              <Navbar.Brand>John L.</Navbar.Brand>
+            </LinkContainer>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className='mr-auto'>
+                <LinkContainer to="/cs428portfolio/">
+                  <Nav.Link>Home</Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="/cs428portfolio/proj1">
+                  <Nav.Link>Project 1</Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="/cs428portfolio/proj2">
+                  <Nav.Link>Project 2</Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="/cs428portfolio/proj3">
+                  <Nav.Link>Project 3</Nav.Link>
+                </LinkContainer>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+
+        <Switch>
+          <Route exact path="/cs428portfolio/" component={Home} />
+          <Route exact path="/cs428portfolio/proj1" component={Proj1} />
+          <Route exact path="/cs428portfolio/proj2" component={Proj2} />
+          <Route exact path="/cs428portfolio/proj3" component={Proj3} />
+        </Switch>
+    </Router>
   );
+}
+
+function Home() {
+  return (
+    <h1>Home</h1>
+  )
+}
+
+function Proj1() {
+  return (
+    <h1>Project 1</h1>
+  )
+}
+
+function Proj2() {
+  return (
+    <h1>Project 2</h1>
+  )
+}
+
+function Proj3() {
+  return (
+    <h1>Project 3</h1>
+  )
 }
 
 export default App;
